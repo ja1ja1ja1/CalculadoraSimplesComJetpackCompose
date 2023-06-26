6b760a829be2599785e29ec9f27259ce12ee4508
ja1ja1ja1 <joaolucasdsouza@gmail.com>
2023-06-26 19:56:30 -0300

    atividade de calculadora simples com jetpack compose

---
+++ app/src/androidTest/java/Calculadora.kt
@@ -0,0 +1,9 @@
+class Calculadora {
+    fun soma (a:Double, b:Double ):Double = a + b
+    fun subtract(a:Double,b:Double):Double = a - b
+    fun multiply(a:Double,b:Double):Double = a * b
+    fun divide(a:Double,b:Double):Double {
+        if(b == 0.0) throw java.lang.IllegalArgumentException("Divisão por zero não pode...")
+        return a / b
+    }
+}
